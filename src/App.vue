9ac6eebf461faabd2fb000e01a956a4df26db274
=caijiale <=caijiale92@163.com>
2017-09-01 10:50:46 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="abc">
+
+    </div>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus">
+  .abc
+    width 100px
+    height 200px
+    background-color red
+</style>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
